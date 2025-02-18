d1593e956a8cd82f779264cb2dd792527be2f8f8
1264188890 <1264188890@qq.com>
2025-02-18 17:37:45 +0800

    修改1

---
+++ pages/studyView/studyIndex.vue
@@ -0,0 +1,73 @@
+<template>
+	<view>
+		<z-swiper
+		  v-model="list"
+		  :options="{slidesPerView : 3,centeredSlides : true,centeredSlidesBounds: true}"
+		>
+		  <z-swiper-item v-for="(item,index) in imgList" :key="index">
+		    <image class="image" :src="item.level" mode="aspectFill"> </image>
+		  </z-swiper-item>
+		</z-swiper>
+	</view>
+</template>
+<!--   <ProgressBar :percentage="30" /> -->
+<script>
+	import ProgressBar from '@/components/progressBar.vue';
+	export default {
+		components: {
+			ProgressBar,
+		},
+		data() {
+			return {
+				imgList: [{
+						level: '基础',
+						imgUrl: '/static/images/基础.png',
+						color: '#DBF3FD'
+					},
+					{
+						level: '初级',
+						imgUrl: '/static/images/初级.png',
+						color: '#CAE9E0'
+					},
+					{
+						level: '中级',
+						imgUrl: '/static/images/中级.png',
+						color: '#FFDCBA'
+					},
+					{
+						level: '高级',
+						imgUrl: '/static/images/高级.png',
+						color: '#D7D4E5'
+					},
+				],
+	
+
+
+			}
+		},
+		computed: {
+
+		},
+		onLoad() {
+			console.log(this, 'this是啥')
+		},
+		onLaunch() {
+			console.log(this, 'this是啥')
+		},
+		mounted() {},
+		methods: {
+			jumpStudyInfo() {
+
+			},
+
+
+
+		}
+	}
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ pages/studyView/studyInfo.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		学习详情
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			console.log(this ,'this是啥')
+
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
